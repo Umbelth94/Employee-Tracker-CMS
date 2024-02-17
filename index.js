1133bceec0d7f7296d4761a1cd00ca155f2b6660
Travis Umbel <Umbelth94@gmail.com>
2024-02-17 16:32:19 -0600

    Added start of inquirer.js file

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config({debug: true})
+console.log(process.env);
+
+console.log(`Hello ${process.env.SQL_PASS}`)
+
+//How should I setup my project structure?  
+
+// Need File For:
+    //Inquirer
+
+//Put
+//Build utility file for queries
+
+//Initialize Inquirer To Prompt
