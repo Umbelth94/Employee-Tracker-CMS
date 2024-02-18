b39367a17745a73dafb1680031dd891bcf9e049d
Travis Umbel <Umbelth94@gmail.com>
2024-02-18 16:16:10 -0600

    Added viewAllEmployees functionality

--- db/seeds.sql
+++ db/seeds.sql
@@ -25,23 +25,30 @@ INSERT INTO roles (title, salary, department_id) VALUES
 INSERT INTO employees (first_name, last_name, role_id) VALUES
 ('John', 'Doe', 1),
 ('Jane', 'Smith', 3), 
-('Michael', 'Johnson', 3), 
-('Patricia', 'Mitchell', 2), 
-('William', 'Wilson', 1), 
+('Michael', 'Johnson', 5), 
+('Patricia', 'Mitchell', 4), 
+('William', 'Wilson', 4), 
 ('Emily', 'Williams', 4),
-('James', 'Scott', 5),
+('James', 'Scott', 2),
 ('Richard', 'Anderson', 6),
 ('David', 'Brown', 7), 
-('Sarah', 'Jones', 8), 
-('Jessica', 'Garcia', 7), 
-('Karen', 'Taylor', 9), 
+('Sarah', 'Jones', 8),
+('Jessica', 'Garcia', 6), 
+('Karen', 'Taylor', 9),
 ('Alice', 'Davis', 1), 
 ('Robert', 'Martinez', 2), 
 ('Laura', 'Hernandez', 4),
 ('Daniel', 'Lopez', 6), 
-('Mary', 'Young', 7), 
+('Mary', 'Young', 6), 
 ('Karen', 'Parker', 8),
-('Matthew', 'Johnson', 9);
+('Matthew', 'Johnson', 10);
 
 # Updating manager id's AFTER the employees table is populated with id's
-UPDATE employees SET manager_id = 1 WHERE id = 13 OR id = 5;
+#Product Manager > Developers, Junior Developers
+UPDATE employees SET manager_id = 2 WHERE id = 1 OR id = 13 OR id = 14 OR id = 7;
+# Marketing Director > Marketing Specialists
+UPDATE employees SET manager_id = 3 WHERE id = 4 OR id = 5 OR id = 6 OR id = 15 OR id = 8 OR id = 11 OR id = 16 OR id = 17;
+#HR Manager > HR Associates
+UPDATE employees SET manager_id = 9 where id = 10 OR id = 18;
+#Maintenance Director > Maintenance Technician
+UPDATE employees SET manager_id = 12 where id = 19;
